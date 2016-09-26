e0b0f13c0b33ef936d93ea24fc60de7b73f323e3
phcamargo23 <phcamargo23@gmail.com>
2016-09-25 23:44:56 -0300

    exemplo base utilizando k-means funcionando

--- kmeans.py
+++ kmeans.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, json, request
 import numpy as np
 from sklearn.cluster import KMeans
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
+
 def main():
-    X = np.array([[1, 2],
-                  [5, 8],
-                  [1.5, 1.8],
-                  [8, 8],
-                  [1, 0.6],
-                  [9, 11]])
+    Z = ['atendimento', 'comida', 'bar'];
+
+    X = [
+        [1, 2, 2],
+        [1, 6, 3],
+        [5, 8, 4],
+        [5, 8, 5],
+        [8, 8, 6],
+        [9, 9, 7]
+    ]
 
     kmeans = KMeans(n_clusters=2)
     kmeans.fit(X)
@@ -21,8 +26,11 @@ def main():
     centroids = kmeans.cluster_centers_
     labels = kmeans.labels_
 
-    # print X
-    # print(labels)
-    # print(centroids)
-    return jsonify(centroids.tolist())
-    # return 'teste'
+    Y = []
+    for i in centroids:
+        Y.append(zip(Z, i))
+
+    return json.dumps(Y)
+
+if __name__ == '__main__':
+    main()
--- static/app.js
+++ static/app.js
@@ -3,10 +3,43 @@
     angular.module('tcc', [])
         .controller('MainController', function ($scope, $http, $filter, $interval, $timeout) {
 
-        // $scope.fit = function() {
-            $http.get('/kmeans').success(function(data){
-                console.log(data);
+            $http.get('/kmeans').success(function (dados) {
+                t = [];
+                angular.forEach(dados, function (value, key) { //Percorre clusters
+                    t.unshift([key.toString(), 'clusters', 0]);
+                    angular.forEach(value, function (v, k) { //Percorre aspecto/centroides
+                        v.splice(1, 0, key.toString());
+                        v[0] = v[0] + '(' + key.toString() + ')';
+                        t.push(v);
+                        // console.log(v);
+                    });
+
+                });
+
+                t.unshift(['clusters', null, 0]);
+                t.unshift(['cluster', 'pai', 'centroide']);
+
+                google.charts.load('current', {'packages': ['treemap']});
+                google.charts.setOnLoadCallback(drawChart);
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable(t);
+
+                    tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
+
+                    var options = {
+                        minColor: '#f00',
+                        midColor: '#ddd',
+                        maxColor: '#0d0',
+                        headerHeight: 15,
+                        fontColor: 'black'
+                    };
+
+                    tree.draw(data, options);
+
+                }
+
+
             });
-        // };
+            // };
         });
 })();
