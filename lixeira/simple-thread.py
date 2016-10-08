fd32e493481b90cccf7dd61b187cf37347455a19
phcamargo23 <phcamargo23@gmail.com>
2016-10-08 01:07:13 -0300

    Implementou MVP para consultar progresso em arquivo.

--- lixeira/simple-thread.py
+++ lixeira/simple-thread.py
@@ -3,24 +3,24 @@ import os
 from flask import Flask, make_response, request, json
 import pandas as pd
 import processamento
+import time
 from threading import Thread
 
-app = Flask(__name__)
-
-
-def analisar(k):
+def myfunc(i):
     dfHeader = ['estado', 'cidade', 'tipo', 'objeto', 'aspectos']
     filename = 'C:\Users\Pedro Henrique\Google Drive\CEULP-ULBRA\TCC II\Lab\dataset3.csv'
-    file_number_rows = len(open(filename).readlines())
+    file_n = len(open(filename).readlines())
+    file_n_total = 0
+    total = 0
+    k = 3
+    chunksize = 10 ** 3
+    for df in pd.read_csv(filename, delimiter=';', names=dfHeader, chunksize=chunksize):
+        file_n_total += len(df)
+        total = file_n_total * 100 / file_n
 
-    file_number_row_current = 0
-    percentual = 0
-
-    for df in pd.read_csv(filename, delimiter=';', names=dfHeader, chunksize=10 ** 3):
-        file_number_row_current += len(df)
-        percentual = file_number_row_current * 100 / file_number_rows
-
-        json.dump({'total': file_number_rows, 'progresso': file_number_row_current, 'percentual': percentual}, open('output/_progresso.json', 'w'))
+        jsonData = json.dumps({'progresso': total})
+        with open('_progesso.json', 'w') as f:
+            json.dump(jsonData, f)
 
         saidaEstado = {}
 
@@ -48,32 +48,13 @@ def analisar(k):
 
             saidaEstado[e].update(saidaCidade);
 
+    # print json.dumps(saidaEstado)
+    # return json.dumps(saidaEstado)
     jsonData = json.dumps(saidaEstado)
-    with open('output/_resultado.json', 'w') as f:
+    with open('_resultado.json', 'w') as f:
         json.dump(jsonData, f)
 
-
-@app.route('/')
-def index():
-    return make_response(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/index.html')).read())
-
-
-@app.route('/consultar')
-def progresso():
-    with open('output/_progresso.json', 'r') as f:
-        data = json.load(f)
-
-    return json.dumps(data)
-
-
-@app.route('/analisar')
-def main():
-    k = int(request.args.get('k'))
-    t = Thread(target=analisar, args=[k])
-    t.start()
-    return 'ok'
-
-
 if __name__ == '__main__':
-    app.run()
-    # main()
+    t = Thread(target=myfunc, args=(8,))
+    t.start()
+    print 'teste'
--- lixeira/thread-flask.py
+++ lixeira/thread-flask.py
@@ -8,10 +8,12 @@ from threading import Thread
 app = Flask(__name__)
 
 
-def analisar(k):
+def myfunc(k):
     dfHeader = ['estado', 'cidade', 'tipo', 'objeto', 'aspectos']
     filename = 'C:\Users\Pedro Henrique\Google Drive\CEULP-ULBRA\TCC II\Lab\dataset3.csv'
     file_number_rows = len(open(filename).readlines())
+
+    # k = 3
 
     file_number_row_current = 0
     percentual = 0
@@ -58,7 +60,7 @@ def index():
     return make_response(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/index.html')).read())
 
 
-@app.route('/consultar')
+@app.route('/progresso')
 def progresso():
     with open('output/_progresso.json', 'r') as f:
         data = json.load(f)
@@ -69,9 +71,9 @@ def progresso():
 @app.route('/analisar')
 def main():
     k = int(request.args.get('k'))
-    t = Thread(target=analisar, args=[k])
+    t = Thread(target=myfunc, args=[k])
     t.start()
-    return 'ok'
+    return ''
 
 
 if __name__ == '__main__':
