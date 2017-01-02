49f46adc1ca047c0aa93e0ca955d37b76f8cdff2
Pedro Henrique <phcamargo23@gmail.com>
2017-01-02 01:32:03 -0300

    Alterações em rótulos

--- principal.py
+++ principal.py
@@ -205,5 +205,5 @@ if __name__ == '__main__':
     if not os.path.exists('output'):
         os.makedirs('output')
 
-    # app.run()
-    analisar('pos_full-sp-rj.csv', 2, 2, 2, 2)
+    app.run()
+    # analisar('pos_full-sp-rj.csv', 2, 2, 2, 2)
